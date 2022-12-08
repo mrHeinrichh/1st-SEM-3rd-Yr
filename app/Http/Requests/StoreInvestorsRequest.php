4829ed99e02e696bddd61ebddfc13ba57f1f2746
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-12-08 12:16:46 +0800

    Add investors store request

---
+++ app/Http/Requests/StoreInvestorsRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreInvestorsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'age' => 'required|string',
+            'contact_number' => 'required|string',
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ];
+    }
+}
